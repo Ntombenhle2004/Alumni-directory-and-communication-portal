a9c6dcc528fdc6d9539e7775705124dfd666d069
Ntando Shezi <ntandop2@gmail.com>
2026-03-11 14:35:51 -0700

    student dashboard

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\user\html\Alumni-directory-and-communication-portal\.venv"
+set "VIRTUAL_ENV=C:\Users\Ntando Shezi\Desktop\Alumni-directory-and-communication-portal\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
